6d34e1ffccd6059857fcf18a749585404c92145e
jaya surya <151901270+jayasuryakalla@users.noreply.github.com>
2025-12-30 14:19:40 +0530

    Delete git_setup.bat

--- git_setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\Program Files\Git\cmd\git.exe" init
-"C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit"
-"C:\Program Files\Git\cmd\git.exe" status
